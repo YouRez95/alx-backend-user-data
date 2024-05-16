1ca95a4ab23e119ea3c203655d41df667dd82621
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-16 14:46:47 +0100

    add typing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ import re
 
 
 def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str):
+                 redaction: str, message: str, separator: str) -> str:
     '''
         function use a regex to replace
         occurrences of certain field value
