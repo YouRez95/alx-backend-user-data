109451243c7606cf7757e1c35e595d86dd6fd830
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-16 14:42:06 +0100

    implement function use regex to hide persanal data

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+'''
+    Regex-ing
+'''
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str):
+    '''
+        function use a regex to replace
+        occurrences of certain field value
+    '''
+    regex = '|'.join('(?<={}=).*?(?=\\{})'.format(field, separator)
+                     for field in fields)
+    return re.sub(regex, redaction, message)
