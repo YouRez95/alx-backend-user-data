0ad02083ff43c8ccf0e3d406412cf10b0f61a9de
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-16 16:03:24 +0100

    format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,30 @@ from typing import List
 import re
 
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+            return the format desired
+        '''
+        msg = super(RedactingFormatter, self).format(record)
+        text = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return text
+
+
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     '''
