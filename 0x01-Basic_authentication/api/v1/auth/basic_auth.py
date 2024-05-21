05e742839e3a63a9f5d24a2a1a5be691c12492d7
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-21 20:29:48 +0100

    add method to get user from credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,10 @@
 import base64
 import binascii
 from api.v1.auth.auth import Auth
-from typing import Tuple
+from typing import Tuple, TypeVar
+
+from models.base import Base
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -60,3 +63,21 @@ class BasicAuth(Auth):
             user_pass = decoded_base64_authorization_header.split(':')
             return (user_pass[0], user_pass[1])
         return (None, None)
+
+    def user_object_from_credentials(
+            self, user_email: str,
+            user_pwd: str) -> TypeVar('User'):  # type: ignore
+        '''
+            returns the User instance based on his email and password
+        '''
+        if (user_email is None or
+                not isinstance(user_email, str) or
+                user_pwd is None or not isinstance(user_pwd, str)):
+            return None
+        user = User.search({'email': user_email})
+        if not user:
+            return None
+        is_valid = user[0].is_valid_password(user_pwd)
+        if is_valid:
+            return user[0]
+        return None
