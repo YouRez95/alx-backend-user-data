7ada289e65cec04803301acbf3ecdeebca2564b3
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-21 13:27:22 +0100

    add extract user credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,7 @@
 import base64
 import binascii
 from api.v1.auth.auth import Auth
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -43,3 +44,19 @@ class BasicAuth(Auth):
             return decoded
         except binascii.Error:
             return None
+
+    def extract_user_credentials(
+                self,
+                decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        '''
+            returns the user email and password from the Base64 decoded value
+        '''
+
+        if (decoded_base64_authorization_header is None or
+                not isinstance(decoded_base64_authorization_header, str)):
+            return None
+
+        if ':' in decoded_base64_authorization_header:
+            user_pass = decoded_base64_authorization_header.split(':')
+            return (user_pass[0], user_pass[1])
+        return None
