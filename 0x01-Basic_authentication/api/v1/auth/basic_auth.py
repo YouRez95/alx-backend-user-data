7c9b62529ae5c65300a52221e27cc0d88fa409cb
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-21 13:18:51 +0100

    add method to decode base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,8 @@
         basic auth
 '''
 
-
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -25,3 +26,20 @@ class BasicAuth(Auth):
             return authorization_header[6:]
 
         return None
+
+    def decode_base64_authorization_header(
+                                    self,
+                                    base64_authorization_header: str) -> str:
+        '''
+            decode the base64 authorization header
+        '''
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decoded
+        except binascii.Error:
+            return None
