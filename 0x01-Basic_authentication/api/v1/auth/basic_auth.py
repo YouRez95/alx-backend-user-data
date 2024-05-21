1f853243c39a734c252d672681840f0eec2db642
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-21 13:03:17 +0100

    add the method base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,4 +13,15 @@ class BasicAuth(Auth):
     '''
         basic auth class
     '''
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        '''
+            extract the base64 from the header
+        '''
+        if (authorization_header is None or
+                not isinstance(authorization_header, str)):
+            return None
+        if authorization_header[:6] == 'Basic ':
+            return authorization_header[6:]
+
+        return None
