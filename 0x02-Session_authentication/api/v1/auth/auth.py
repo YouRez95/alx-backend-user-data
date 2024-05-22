daea1d9a5e1446f891c05e136fabfe462a1cd286
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-22 11:19:59 +0100

    add the current user to request

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+'''
+    class:
+        Auth
+'''
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    '''
+        class manage the API authentication
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+            require auth
+        '''
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path[-1:] != '/':
+            second_path = path + '/'
+            if second_path in excluded_paths:
+                return False
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        '''
+            authorization header
+        '''
+        if request is None:
+            return None
+        value = request.headers.get('Authorization')
+        return value
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        '''
+            current user
+        '''
+        return None
