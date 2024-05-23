4758724db4ffc97567337a25953f40432b82dd8b
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-23 15:50:47 +0100

    add comment to method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,6 +17,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        '''
+            method that can create session
+        '''
         if user_id is None or type(user_id) is not str:
             return None
 
