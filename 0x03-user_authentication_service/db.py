98b9700cd38fe80f3eda226bc6dce40524c76b00
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-28 10:37:42 +0100

    check the email and password in add user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,6 +35,8 @@ class DB:
         '''
             add new user to database
         '''
+        if email is None or hashed_password is None:
+            return
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
