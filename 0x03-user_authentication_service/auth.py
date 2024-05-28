d31240d35f7487e413ad450751d19cc2195baa38
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-28 12:39:59 +0100

    add one line to hash_password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -10,7 +10,4 @@ def _hash_password(plain_text_pass: str) -> str:
     '''
         hash the password
     '''
-    bytes = plain_text_pass.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(bytes, salt)
-    return hash
+    return bcrypt.hashpw(plain_text_pass.encode(), bcrypt.gensalt())
