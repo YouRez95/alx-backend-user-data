95fdc74706afc6e7ca0443ae563f0b4daf6e0732
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-28 12:32:48 +0100

    adding method to hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    auth module
+"""
+
+import bcrypt
+
+
+def _hash_password(plain_text_pass):
+    '''
+        hash the password
+    '''
+    bytes = plain_text_pass.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
