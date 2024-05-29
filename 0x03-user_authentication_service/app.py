0863d7333b17090c2ebfe71f3bc68d03caddeeff
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-29 22:06:10 +0100

    create the session route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
     flask app
 '''
 
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 from auth import Auth
 
@@ -33,6 +33,24 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    '''
+        route that can login user  and add sessionid to coockies
+        form data required:
+            email, password
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_credentials = AUTH.valid_login(email, password)
+    if not valid_credentials:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
