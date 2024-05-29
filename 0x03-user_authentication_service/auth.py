2efef73908ffc979acc6b26cbca702d883f42d0c
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-29 22:10:50 +0100

    get user from session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,14 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User | None:
+        '''
+            search the user by session id
+        '''
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
